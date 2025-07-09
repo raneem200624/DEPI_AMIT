def614c1a474e9340fda7bc70e935212bf1d3413
raneem200624 <raneemamr11@gmail.com>
2025-07-09 23:55:56 +0300

    done

---
+++ src/ass_2_a.java
@@ -0,0 +1,34 @@
+import java.util.Scanner;
+
+public class ass_2_a {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the number of students:");
+        int x=s.nextInt();
+        s.nextLine();
+        for(int i=1;i<=x;i++){
+            System.out.print("Enter the student's name:");
+            String name=s.nextLine();
+           // s.nextLine();
+            //System.out.println();
+            System.out.print("Enter three exam scores:");
+            double y1=s.nextDouble();
+            double y2=s.nextDouble();
+            double y3=s.nextDouble();
+            double avg =(y1+y2+y3)/3;
+
+            System.out.println("Name:" + name);
+            System.out.println("Average:" + avg);
+
+            if( avg>=85)
+                System.out.println("Grade: A");
+            else if( avg>=70)
+                System.out.println("Grade: B");
+            else if( avg>=50)
+                System.out.println("Grade: C");
+            else
+                System.out.println("Grade: F");
+            s.nextLine();
+        }
+    }
+}
